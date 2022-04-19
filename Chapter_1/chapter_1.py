0c982bd2a5a28857daf5656ffca4e8bbfcdf4eae
Jack <jack.center@picknik.ai>
2022-04-19 16:22:00 -0600

    first commit

---
+++ Chapter_1/chapter_1.py
@@ -0,0 +1,22 @@
+import exercise_1, exercise_2
+
+
+def main():
+    print_header()
+    exercise_1.main()
+    exercise_2.main()
+
+
+def print_header():
+    print("=====================================================")
+    print("=====================================================")
+    print("                     Chapter 1:")
+    print("                     TODO: Title")
+    print("                     Jack Center")
+    print("=====================================================")
+    print("=====================================================")
+    print()
+
+
+if __name__ == "__main__":
+    main()
