45cfe523f54d78e9e6b6944f584f5aaeaa390e99
Jack <jack.center@picknik.ai>
2022-04-22 14:26:03 -0600

    Added exercise 4.

--- Chapter_1/chapter_1.py
+++ Chapter_1/chapter_1.py
@@ -1,4 +1,4 @@
-import exercise_1, exercise_2, exercise_3
+import exercise_1, exercise_2, exercise_3, exercise_4
 
 
 def main():
@@ -6,6 +6,7 @@ def main():
     exercise_1.main()
     exercise_2.main()
     exercise_3.main()
+    exercise_4.main()
 
 
 def print_header():
